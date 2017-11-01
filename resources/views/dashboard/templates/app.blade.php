190c7c98ab07b0dacc0b1ae5805bfe76eff97452
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2017-11-01 09:46:41 -0200

    mac-commit 135

--- resources/views/dashboard/templates/app.blade.php
+++ resources/views/dashboard/templates/app.blade.php
@@ -280,24 +280,23 @@
                         $umdiaantes = $datatual->modify('-1 day');
 
                     @endphp
-                    <div class="row">
-                        <div class="col s12 m12 l12 ">
-                            <div class="card yellow">
-                                <div class="card-content">
-                                    <span class="card-title center"><b>Aviso</b></span>
-                                    <p class="flow">Sua mensalidade venceu em: <b>{{Carbon\Carbon::parse($validaMesalidade->datven)->format('d/m/Y')}}</b>.</p>
-                                    <p class="flow">Evite o bloquei do acesso efetuando o pagamento até o dia: <b>{{Carbon\Carbon::parse($validaMesalidade->datpro)->modify('-1 day')->format('d/m/Y')}}</b>.</p>
-                                    <p class="flow">Para maiores informações falar com setor financeiro</p>
-                                    <p class="flow">Data para bloquieio: <b>{{Carbon\Carbon::parse($validaMesalidade->datpro)->format('d/m/Y')}}</b></p>
+                    @if($datven < $datatual)
+                        <div class="row">
+                            <div class="col s12 m12 l12 ">
+                                <div class="card yellow">
+                                    <div class="card-content">
+                                        <span class="card-title center"><b>Aviso</b></span>
+                                        <p class="flow">Sua mensalidade venceu em: <b>{{Carbon\Carbon::parse($validaMesalidade->datven)->format('d/m/Y')}}</b>.</p>
+                                        <p class="flow">Evite o bloquei do acesso efetuando o pagamento até o dia: <b>{{Carbon\Carbon::parse($validaMesalidade->datpro)->modify('-1 day')->format('d/m/Y')}}</b>.</p>
+                                        <p class="flow">Para maiores informações falar com setor financeiro</p>
+                                        <p class="flow">Data para bloquieio: <b>{{Carbon\Carbon::parse($validaMesalidade->datpro)->format('d/m/Y')}}</b></p>
+                                        <p></p>
+                                    </div>
 
-                                    <p></p>
                                 </div>
-
                             </div>
                         </div>
-
-                    </div>
-
+                    @endif
 
 
                 @endif
